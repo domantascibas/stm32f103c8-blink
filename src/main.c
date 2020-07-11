e4a6d6728bb9bb15ee3dcae42a3481986986209d
Domantas Cibas <domantas@nectarsun.com>
2020-07-11 11:16:55 +0300

    blink PC13 not PB0

--- src/main.c
+++ src/main.c
@@ -6,21 +6,19 @@
 
 #include "stm32f1xx.h"
 
-int main(void)
-{
+int main(void) {
     //Step 1: Enable the clock to PORT B
-    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 
-    //Step 2: Change PB0's mode to 0x3 (output) and cfg to 0x0 (push-pull)
-    GPIOB->CRL = GPIO_CRL_MODE0_0 | GPIO_CRL_MODE0_1;
+    //Step 2: Change PC13's mode to 0x3 (output) and cfg to 0x0 (push-pull)
+    GPIOC->CRH = GPIO_CRH_MODE13_0 | GPIO_CRH_MODE13_1;
 
-    while (1)
-    {
-        //Step 3: Set PB0 high
-        GPIOB->BSRR = GPIO_BSRR_BS0;
+    while (1) {
+        //Step 3: Set PC13 high
+        GPIOC->BSRR = GPIO_BSRR_BS13;
         for (uint16_t i = 0; i != 0xffff; i++) { }
-        //Step 4: Reset PB0 low
-        GPIOB->BSRR = GPIO_BSRR_BR0;
+        //Step 4: Reset PC13 low
+        GPIOC->BSRR = GPIO_BSRR_BR13;
         for (uint16_t i = 0; i != 0xffff; i++) { }
     }
 
